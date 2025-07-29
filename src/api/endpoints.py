22bb3e45c4857004499ef410a2ec1b5c82ac4505
Matias-Sanchez-Wilson <92598449+Matias-Sanchez-Wilson@users.noreply.github.com>
2025-07-29 19:07:43 +0100

    refactor: reorder imports and format exchange function call

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,12 +1,16 @@
-from scripts.utils import exchange
-from scripts.jets import F16, F18
 from fastapi import APIRouter
 
+from scripts.jets import F16, F18
+from scripts.utils import exchange
+
 router = APIRouter()
+
 
 @router.get("/dogfight/exchange/{distance}/{duration}")
 def exchange_endpoint(distance: int, duration: int):
     fighter1 = F16()
     fighter2 = F18()
-    post_exchange = exchange(fighter1=fighter1, fighter2=fighter2, distance=distance, duration=duration)
+    post_exchange = exchange(
+        fighter1=fighter1, fighter2=fighter2, distance=distance, duration=duration
+    )
     return post_exchange
